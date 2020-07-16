fc014c808f8a8c14af950eee588765209eaadd2f
chorseng <39493602+chorseng@users.noreply.github.com>
2020-07-16 18:23:46 +0800

    Update eval_simmc_agent.py

--- mm_action_prediction/eval_simmc_agent.py
+++ mm_action_prediction/eval_simmc_agent.py
@@ -33,9 +33,7 @@ def main(args):
         "data_read_path": args["eval_data_path"],
         "get_retrieval_candidates": True,
     }
-    print(dataloader_args)
     dataloader_args.update(saved_args)
-    print(dataloader_args)
     val_loader = loaders.DataloaderSIMMC(dataloader_args)
     saved_args.update(val_loader.get_data_related_arguments())
 
