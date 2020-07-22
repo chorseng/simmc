ad724dc70fb2414e14bfa3550f5b6a9b1b0fa7b7
chorseng <39493602+chorseng@users.noreply.github.com>
2020-07-22 17:00:39 +0800

    Update train_simmc_model.sh

--- mm_action_prediction/scripts/train_simmc_model.sh
+++ mm_action_prediction/scripts/train_simmc_model.sh
@@ -43,9 +43,9 @@ COMMON_FLAGS="
 
 # Train history-agnostic model.
 # For other models, please look at scripts/train_all_simmc_models.sh
-python -u train_simmc_agent.py $COMMON_FLAGS \
-    --encoder="memory_network" \
-    --text_encoder="lstm"
+#python -u train_simmc_agent.py $COMMON_FLAGS \
+#    --encoder="memory_network" \
+#    --text_encoder="lstm"
     #--encoder="history_agnostic" \
     
 
@@ -54,7 +54,7 @@ python -u train_simmc_agent.py $COMMON_FLAGS \
 
 # Evaluate a trained model checkpoint.
 #CHECKPOINT_PATH="checkpoints/hae/epoch_20.tar"
-#CHECKPOINT_PATH="checkpoints/epoch_30.tar"
-#python -u eval_simmc_agent.py  \
-#    --eval_data_path=${DEV_JSON_FILE/.json/_mm_inputs.npy} \
-#    --checkpoint="$CHECKPOINT_PATH" --gpu_id=${GPU_ID} --batch_size=50 --domain=${DOMAIN}
+CHECKPOINT_PATH="checkpoints/epoch_25.tar"
+python -u eval_simmc_agent.py  \
+    --eval_data_path=${DEV_JSON_FILE/.json/_mm_inputs.npy} \
+    --checkpoint="$CHECKPOINT_PATH" --gpu_id=${GPU_ID} --batch_size=50 --domain=${DOMAIN}
